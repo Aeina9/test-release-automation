35eb15b9382815caf79e83e99528ac2cc9aba280
Aeina9 <ixamynguyen@gmail.com>
2023-08-21 14:17:30 -0500

    Add test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import TestElement from './testElement';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Pioneer!
-        </p>
+        <TestElement/>
         <a
           className="App-link"
           href="https://reactjs.org"
