d5bd825a9950df5280a25fb494c30228bed6cce9
Aeina9 <ixamynguyen@gmail.com>
2023-08-28 13:50:21 -0500

    change 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TestElement from './testElement';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <TestElement/>
         <a
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </h1>
     </div>
   );
 }
